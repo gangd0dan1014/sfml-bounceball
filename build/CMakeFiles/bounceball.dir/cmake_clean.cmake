4aadd580f8c4d3ea941563cbb6aa9a07436a0f52
gangd0dan1014 <kmj26535@gmail.com>
2025-08-11 14:31:30 +0900

    initial commit

---
+++ build/CMakeFiles/bounceball.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bounceball.dir/src/main.cpp.o"
+  "CMakeFiles/bounceball.dir/src/main.cpp.o.d"
+  "bounceball"
+  "bounceball.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bounceball.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
