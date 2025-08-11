b060812c982dd0dfd6774790718020ff7b80b478
gangd0dan1014 <kmj26535@gmail.com>
2025-08-11 16:37:21 +0900

    feat: 중력 및 자유 낙하 구현

---
+++ include/Ball.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Ball : public sf::Drawable, public sf::Transformable {
+
+    private:
+        sf::Vector2f initPosition;
+        sf::Vector2f position;
+        sf::Vector2f velocity;
+        sf::Vector2f gravity;
+
+        sf::CircleShape ball;
+
+        void initSetting();
+
+    public:
+        void updatePosition(float dt);
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+        Ball(sf::Vector2f startPosition);
+
+
+};
