a01fd1d87a6b05bdf51153ad530f4d769af2ab79
gangd0dan1014 <kmj26535@gmail.com>
2025-08-12 10:33:19 +0900

    feat: 베이스 블럭 객체 구현

---
+++ src/Block.cpp
@@ -0,0 +1,51 @@
+#include "Block.hpp"
+
+/**
+ * @param x 시작 x좌표
+ * @param y 시작 y 좌표
+ * @param width x방향 블럭의 개수
+ * @param height y방향 블럭의 개수
+ * @param image 블럭에 적용될 이미지 상대 경로
+ * @brief 블럭 생성자
+ */
+Block::Block(int x, int y, int width, int height, std::string image) 
+    :position(x * BLOCK_SIZE,y * BLOCK_SIZE), blockCount(width, height), imageRelativePath(image) {
+    initSetting();
+}
+
+/**
+ * @brief 블럭을 만들기 위한 기본 세팅
+ * @details 블럭을 만들기 위한 함수들을 모아둔 함수
+ */
+void Block::initSetting() {
+    if (!texture.loadFromFile(imageRelativePath)) {
+        std::cout << imageRelativePath << ' ' << "이미지 로딩 실패";
+    }
+    createRenderTexture();
+    sprite.setPosition(position);
+}
+
+/**
+ * @brief 블럭을 가상공간에 그리는 함수
+ * @details 블럭을 가상공간에 그리고, 저장하는 함수
+ */
+void Block::createRenderTexture() {
+    renderTexture.create(blockCount.x * BLOCK_SIZE , blockCount.y * BLOCK_SIZE);
+
+    for (int y = 0; y < blockCount.y; y++) {
+        for (int x = 0; x < blockCount.x; x++) {
+            sf::Sprite temp(texture);
+            temp.setPosition(x * BLOCK_SIZE, y * BLOCK_SIZE);
+            renderTexture.draw(temp);
+        }
+    }
+
+    renderTexture.display();
+    sprite.setTexture(renderTexture.getTexture());
+}
+
+void Block::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    states.transform *= getTransform();
+    target.draw(sprite, states);
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Block.hpp"
 
 int main() {
 
-    std::cout << "Hello World" << std::endl;
+    sf::RenderWindow window;
+    window.create(sf::VideoMode(2048.f, 1024.f), "Bounce Ball");
 
+    sf::Event event;
+
+    BackGround backGround = BackGround();
+
+    while (window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(backGround);
+        window.display();
+    }
 }
